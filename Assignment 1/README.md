0e44835aa53d88824d4290c15553025676738dcc
Crystal Liles <CLiles26@hotmail.com>
2019-10-02 06:39:40 -0500

    Final edit for Assignment 1

---
+++ Assignment 1/README.md
@@ -0,0 +1,1 @@
+# Assignments - Crystal Liles
---
+++ Assignment 1/Topical Assignment 1/CustomerManagement/Customer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * Crystal Liles
+ */
+
+namespace CustomerManagement
+{
+    public class Customer
+    {
+        public int CustomerID { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(FirstName) &&
+                    !string.IsNullOrWhiteSpace(LastName))
+                {
+                    return $"{LastName}, {FirstName}";
+                }
+                return LastName + FirstName;
+            }
+        }
+        public static int InstanceCount { get; set; }
+        private string _lastName;
+
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+        ///<summary>
+        /// Validates the customer data.
+        /// </summary>
+        ///<returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+
+        ///<summary>
+        /// Retrieve one customer.
+        /// </summary>
+        ///<returns></returns>
+        public Customer Retreive(int customerID)
+        {
+            // Code that retrieves the defined customer
+
+            return new Customer();
+        }
+
+        ///<summary>
+        /// Retrieve all customers.
+        /// </summary>
+        ///<returns></returns>
+        public List<Customer> Retreive()
+        {
+            // Code that retrieves all of the customers
+
+            return new List<Customer>();
+        }
+
+        ///<summary>
+        /// Saves the current customer.
+        /// </summary>
+        ///<returns></returns>
+        public bool Save()
+        {
+            //Code that saves the defined cusotmer
+
+            return true;
+
+        }
+    }
+}
