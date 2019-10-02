319662f14c0daf76afc76cc461a2dc57e858f6b6
Crystal Liles <CLiles26@hotmail.com>
2019-10-01 21:42:17 -0500

    10/1/2019

--- Topical Assignment 1/CustomerTest/CustomerTest.cs
+++ Topical Assignment 1/CustomerTest/CustomerTest.cs
@@ -79,5 +79,42 @@ namespace CustomerTest
             //-- Assert
             Assert.AreEqual(3, Customer.InstanceCount);
         }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+            //-- Arrange
+            var customer = new Customer
+            {
+                LastName = "Crystal",
+                EmailAddress = "crallen@my.lonestar,edu"
+            };
+
+            var expected = true;
+
+            //-- Act
+            var actual = customer.Validate();
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateMissingLastName()
+        {
+            //-- Arrange
+            var customer = new Customer
+            {
+                EmailAddress = "crallen@my.lonestar,edu"
+            };
+
+            var expected = false;
+
+            //-- Act
+            var actual = customer.Validate();
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
