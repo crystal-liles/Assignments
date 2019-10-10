18d621e881ebbc5494a42916af4b11bd701b7c52
Crystal Liles <CLiles26@hotmail.com>
2019-10-09 22:42:20 -0500

    Add Extension Method and Tests to Master Project

---
+++ MasterAssignment/CommonFramework.Core.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿using CommonFramework.Core;
+using Shouldly;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ *Crystal Liles
+ */
+
+namespace CommonFramework.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        #region IsNullOrEmpty
+
+        [TestCase]
+            public void IsNullOrEmpty_IsNull()
+            {
+                // Arrange
+                string testCondition = null;
+
+                //Act
+                var actualResult = testCondition.IsNullOrEmpty();
+
+                //Assert
+                actualResult.ShouldBeTrue();
+            }
+
+        [TestCase]
+            public void IsNullOrEmpty_IsEmpty()
+            {
+                // Arrange
+                string testCondition = "";
+
+                //Act
+                var actualResult = testCondition.IsNullOrEmpty();
+
+                //Assert
+                actualResult.ShouldBeTrue();
+            }
+
+        [TestCase]
+            public void IsNullOrEmpty_IsSpaces()
+            {
+            // Arrange
+            string testCondition = "";
+
+                //Act
+                var actualResult = testCondition.IsNullOrEmpty();
+
+                //Assert
+                actualResult.ShouldBeTrue();
+            }
+
+        [TestCase]
+            public void IsNullOrEmpty_HasContent()
+            {
+                // Arrange
+                string testCondition = "Hello";
+
+                //Act
+                var actualResult = testCondition.IsNullOrEmpty();
+
+                //Assert
+                actualResult.ShouldBeFalse();
+            }
+
+            #endregion
+
+            #region IsNullOrWhiteSpace
+
+            #endregion
+
+            #region Left
+
+            #endregion
+
+            #region Right
+
+            #endregion
+        
+
+    }
+}
