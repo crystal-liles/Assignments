c9f4dfd273f75e284a4f4e079d0a3c66640ce53d
Crystal Liles <CLiles26@hotmail.com>
2019-10-10 21:26:47 -0500

    Add Enum Class and GameCell Form.
    Add CanvasGameCells

---
+++ Battleship/BattleshipUI/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * Crystal Liles
+ */
+
+namespace BattleshipUI
+{
+    class Enums
+    {
+        ///<summary>
+        /// The ShipCellSegment enum represents to possible content
+        /// of each cell (grid component) in the game.
+        /// </summary>
+        public enum ShipCellSegment
+        {
+            Water,
+            Single,
+            Left,
+            Right,
+            Top,
+            Bottom,
+            Vertical,
+            Horizontal
+        }
+
+    }
+}
---
+++ Battleship/BattleshipUI/GameCell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+ * Crystal Liles
+ */
+
+namespace BattleshipUI
+{
+    public partial class GameCell : UserControl
+    {
+        public GameCell()
+        {
+            InitializeComponent();
+        }
+
+        public int GameCellRow { get; set; }
+        public int GameCellColumn { get; set; }
+        public ShipCellSegment { get; set; }
+
+    }
+}
