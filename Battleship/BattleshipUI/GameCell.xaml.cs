e2e44302422d8ee8b2feb87ebbe0c2928363f703
Crystal Liles <CLiles26@hotmail.com>
2019-10-30 20:46:12 -0500

    Additions to Game Cell

--- Battleship/BattleshipUI/GameCell.xaml.cs
+++ Battleship/BattleshipUI/GameCell.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Security.Policy;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 
 /*
@@ -8,15 +12,67 @@
 namespace BattleshipUI
 {
     public partial class GameCell : UserControl
-    { 
-
+    {
+        public GameCell()
+        {
+            InitializeComponent();
+        }
         public int GameCellRow { get; set; }
         public int GameCellColumn { get; set; }
-        public ShipCellSegment ShipSegment { get; set; }
+        private bool _shipSegmentShown;
 
+        public bool ShipSegmentShown
+        {
+            get => _shipSegmentShown;
+            set
+            {
+                _shipSegmentShown = value;
+                DisplayButtonBackground();
+            }
+        }
+
+        private ShipCellSegment _shipSegment = ShipCellSegment.Unknown;
+        public ShipCellSegment ShipSegment
+        {
+            get => _shipSegmentShown;
+            set
+            {
+                _shipSegmentShown = value;
+                DisplayButtonBackground();
+            }
+        }
+
+        private void DisplayButtonBackground()
+        {
+            if (_shipSegmentShown)
+            {
+                switch (_shipSegment)
+                {
+                    case ShipCellSegment.Unknown:
+                    {
+                        CalendarDateChangedEventArgs uri = _shipSegmentUri[ShipCellSegmetn.Unkown];
+                        var bitmapImage = new BitmapImage(uri);
+                        var brush = new ImageBrush();
+                        brush.ImageSource = bitmapImage;
+                        brush.Stretch = Stretch.Fill;
+                        BtnCellSegment.Background = brush;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                var uri = new Uri("Images/battleship_unknown.png", UriKind.Relative);
+                var bitmapImage = new BitmapImage(uri);
+                var brush = new ImageBrush();
+                brush.ImageSource = bitmapImage;
+                brush.Stretch = Stretch.Fill;
+                BtnCellSegment.Background = brush;
+            }
+        }
         private void BtnCellSegment_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-
+            ShipSegmentShown = true;
         }
     }
 
--- MasterAssignment/AssignmentDemonstratorWinForm/MainForm.Designer.cs
+++ MasterAssignment/AssignmentDemonstratorWinForm/MainForm.Designer.cs
@@ -31,16 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.MenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.TxtFilePath = new System.Windows.Forms.TextBox();
             this.ListBoxOutput = new System.Windows.Forms.ListBox();
+            this.TxtFilePath = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -71,19 +71,24 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(80, 41);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // MenuItemHelp
-            // 
-            this.MenuItemHelp.Name = "MenuItemHelp";
-            this.MenuItemHelp.Size = new System.Drawing.Size(403, 48);
-            this.MenuItemHelp.Text = "E&xit";
-            this.MenuItemHelp.Click += new System.EventHandler(this.MenuItemHelp_Click);
-            // 
             // MenuItemOpen
             // 
             this.MenuItemOpen.Name = "MenuItemOpen";
-            this.MenuItemOpen.Size = new System.Drawing.Size(403, 48);
+            this.MenuItemOpen.Size = new System.Drawing.Size(232, 48);
             this.MenuItemOpen.Text = "Open";
             this.MenuItemOpen.Click += new System.EventHandler(this.MenuItemOpen_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(229, 6);
+            // 
+            // MenuItemHelp
+            // 
+            this.MenuItemHelp.Name = "MenuItemHelp";
+            this.MenuItemHelp.Size = new System.Drawing.Size(232, 48);
+            this.MenuItemHelp.Text = "E&xit";
+            this.MenuItemHelp.Click += new System.EventHandler(this.MenuItemHelp_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -117,11 +122,6 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(347, 37);
             this.toolStripStatusLabel1.Text = "Status and Information Strip";
             // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(400, 6);
-            // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -136,6 +136,16 @@
             this.splitContainer1.SplitterDistance = 347;
             this.splitContainer1.TabIndex = 2;
             // 
+            // ListBoxOutput
+            // 
+            this.ListBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ListBoxOutput.FormattingEnabled = true;
+            this.ListBoxOutput.ItemHeight = 29;
+            this.ListBoxOutput.Location = new System.Drawing.Point(0, 523);
+            this.ListBoxOutput.Name = "ListBoxOutput";
+            this.ListBoxOutput.Size = new System.Drawing.Size(691, 3);
+            this.ListBoxOutput.TabIndex = 1;
+            // 
             // TxtFilePath
             // 
             this.TxtFilePath.Dock = System.Windows.Forms.DockStyle.Top;
@@ -146,16 +156,7 @@
             this.TxtFilePath.ReadOnly = true;
             this.TxtFilePath.Size = new System.Drawing.Size(691, 523);
             this.TxtFilePath.TabIndex = 0;
-            // 
-            // ListBoxOutput
-            // 
-            this.ListBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ListBoxOutput.FormattingEnabled = true;
-            this.ListBoxOutput.ItemHeight = 29;
-            this.ListBoxOutput.Location = new System.Drawing.Point(0, 523);
-            this.ListBoxOutput.Name = "ListBoxOutput";
-            this.ListBoxOutput.Size = new System.Drawing.Size(691, 3);
-            this.ListBoxOutput.TabIndex = 1;
+            this.TxtFilePath.Text = "File Path to be seen here.";
             // 
             // Form1
             // 
